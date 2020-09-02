77010568da1575f104bfb17b8750244fb968ae2b
gmujica <gregorymujica1993@gmail.com>
2020-09-02 18:27:45 -0500

    changes

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const useSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        displayName: {
-            type: String
-        }
-    }
-})
+const userSchema = new mongoose.Schema({
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true, 
+    minlength: 5 
+  },
+  displayName: { 
+    type: String 
+  },
+});
 
-module.exports = User = mongoose.model("user", useSchema)
+module.exports = User = mongoose.model("user", userSchema);
